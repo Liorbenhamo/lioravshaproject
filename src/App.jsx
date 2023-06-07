27c7f8d05e461a61eaa06ace8bb1f07256e19b2f
avshalom evgi <avsha.em@gmail.com>
2023-06-08 00:08:13 +0300

    firt commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { createContext, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "../component/Navbar";
-import Homepage from "../pages/Homepage";
+import HomePage from '../component/homePage/HomePage'
 import Report from "../pages/Report";
+import TeamInfo from '../component/teamInfo/TeamInfo';
 export const InfoContext = createContext();
 function App() {
   const [correntuser, setCorrentuser] = useState();
@@ -17,8 +18,10 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Navbar />}>
-              <Route path="/" element={<Homepage />}></Route>
+              <Route index element={<HomePage />}></Route>
               <Route path="/report" element={<Report />}></Route>
+              <Route path="team/:id" element={<TeamInfo/>}></Route>
+
             </Route>
           </Routes>
         </BrowserRouter>
