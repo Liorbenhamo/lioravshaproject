21c5e6fc35161f5419ef6b91cbc0b9dcefb62518
avshalom evgi <avsha.em@gmail.com>
2023-06-08 03:26:14 +0300

    commit2

--- component/Navbar.jsx
+++ component/Navbar.jsx
@@ -28,7 +28,7 @@ function Navbar() {
       <header className="headernavbar">
         <Link to="/">
           <h1 className="logo">
-            <p>bat yam school</p>
+            <p>Kfar Galim high school</p>
           </h1>
         </Link>
 
@@ -66,6 +66,78 @@ function Navbar() {
         </ul>
       </header>
       <Outlet />
+      <div className="footer">
+        <div>
+          <h1 className="headers">LOCATION</h1>
+          <div class="mapouter">
+            <div class="gmap_canvas">
+              <iframe
+                width="250"
+                height="150"
+                id="gmap_canvas"
+                src="https://maps.google.com/maps?q=%D7%9B%D7%A4%D7%A8%20%D7%92%D7%9C%D7%99%D7%9D&t=&z=15&ie=UTF8&iwloc=&output=embed"
+                frameborder="0"
+                scrolling="no"
+                marginheight="0"
+                marginwidth="0"
+              ></iframe>
+              <a href="https://embedgooglemap.net/124/"></a>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h1 className="headers">USEFUL PHONE NUMBERS</h1>
+          <p className="footer-text">Principle - 0584664304</p>
+          <p className="footer-text">Mazcir - 0584664304</p>
+          <p className="footer-text">MB - 0584664304</p>
+          <p className="footer-text">Mancal - 0584664304</p>
+        </div>
+        <div>
+          <h1 className="headers">FIND US ON</h1>
+
+          <div></div>
+          <a
+            className="footer-text"
+            target="_blank"
+            href="https://www.youtube.com/channel/UCZipgpiwZ-2c8uQHQrqnSog"
+          >
+            <img
+              className="logos"
+              src="https://www.liblogo.com/img-logo/yo482f28b-youtube-icon-logo-free-youtube-logo-icon-symbol-png-svg-download.png"
+              alt="image"
+            />
+          </a>
+          <br></br>
+          <a
+            className="footer-text"
+            target="_blank"
+            href="https://www.facebook.com/kfargalim.kfargalim/"
+          >
+            <img
+              className="logos"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Facebook_icon_2013.svg/640px-Facebook_icon_2013.svg.png"
+              alt="image"
+            />
+          </a>
+          <br></br>
+          <a
+            className="footer-text"
+            target="_blank"
+            href="https://www.instagram.com/kfar.galim/?hl=en"
+          >
+            <img
+              className="logos"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/640px-Instagram_logo_2022.svg.png"
+              alt="image"
+            />
+          </a>
+          <br></br>
+        </div>
+        <div>
+          <h1 className="headers">CONTACT US</h1>
+          <p className="footer-text">contact us in the page</p>
+        </div>
+      </div>
     </div>
   );
 }
--- component/homePage/HomePage.jsx
+++ component/homePage/HomePage.jsx
@@ -11,32 +11,79 @@ import {
   faChess,
   faFutbol,
   faBasketballBall,
+  faEarthAmericas,
+  faBridge,
+  faPerson
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./homePage.css";
 import Team from "../teams/Team";
 import teamData from "../../teamData.json";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function HomePage() {
-  const colors=['yellow','black','green','brown']
+  const colors = ["yellow", "black", "green", "brown"];
   const data = cardData.data;
   const teamNData = teamData.teamData;
-  const teamIcons = [
-    faVolleyballBall,
-    faChess,
-    faFutbol ,
-    faBasketballBall,
+  const teamIcons = [faVolleyballBall, faChess, faFutbol, faBasketballBall];
+
+  const [currentImage, setCurrentImage] = useState(0);
+  const images = [
+    "../../pictures/1.jpg",
+    "../../pictures/2.jpg",
+    "../../pictures/3.jpg",
+    "../../pictures/4.jpg",
   ];
-  
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }, 5000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div>
       <div className="news-ticker">
         <NewsTicker></NewsTicker>
       </div>
+      <div className="head-contaiter">
+        <div className="background-image">
+          {" "}
+          <img src={images[currentImage]} alt="Slider Image" />
+        </div>
+        <div className="headline-school">
+          <h1>Kfar Galim high school</h1>
+          <h3>A place of love and excellence</h3>
+          <FontAwesomeIcon
+            icon={faGraduationCap}
+            size="2xl"
+            style={{ color: "#2d22c3" }}
+          />
+        </div>
+      </div>
+      <div className="container-card">
+        <div style={{ backgroundColor: "orange" }} className="cards">
+         <h1>Pleasure in paddling</h1> 
+        <div> <FontAwesomeIcon icon={faEarthAmericas} size="2xl" color="white" /></div> 
+        </div>
+        <div style={{ backgroundColor: "blue" }} className="cards">
+         <h1>A bond as a bridge</h1>
+          <div><FontAwesomeIcon icon={faBridge} size="2xl" color="white" /></div>
+        </div>
+        <div style={{ backgroundColor: "green" }} className="cards">
+         <h1> place in the world</h1>
+         <div> <FontAwesomeIcon icon={faPerson} size="2xl" color="white" /></div>
+        </div>
+      </div>
       <br />
       <section className="homePage-section">
         <div className="homePage-container">
+          <h1 className="headline-carousel">Main Event</h1>
+          <hr />
           <div className="events-carousel">
             <Carousel show={2} infiniteLoop={true}>
               {data.map((item) => (
@@ -50,34 +97,35 @@ function HomePage() {
               ))}
             </Carousel>
           </div>
-          <div>
-            <ul>
-              <li>
-                {" "}
-                <FontAwesomeIcon icon={faUser} />
-                Personal attention{" "}
-              </li>
-              <li>
-                {" "}
-                <FontAwesomeIcon icon={faGraduationCap} />
-                Professional and dedicated educators{" "}
-              </li>
-              <li>
-                {" "}
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
-                Education for values and imparting knowledge
-              </li>
-            </ul>
+          <div className="feature-list">
+            <div className="feature-item">
+              <FontAwesomeIcon icon={faUser} />
+              <span>Personal attention</span>
+            </div>
+            <div className="feature-item">
+              <FontAwesomeIcon icon={faGraduationCap} />
+              <span>Professional and dedicated educators</span>
+            </div>
+            <div className="feature-item">
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+              <span>Education for values and imparting knowledge</span>
+            </div>
           </div>
+
           <div className="teams-container">
             {teamNData &&
               teamNData.map((value, index) => {
                 const color = colors[index % colors.length];
-                
+
                 return (
                   <div key={index} className="team-item">
-                    <Link  to={`/team/${value.id}`}>
-                    <Team   icon={teamIcons[index % teamIcons.length]} style={{ backgroundColor: color }} title={value.title} image={value.image} />
+                    <Link to={`/team/${value.id}`}>
+                      <Team
+                        icon={teamIcons[index % teamIcons.length]}
+                        style={{ backgroundColor: color }}
+                        title={value.title}
+                        image={value.image}
+                      />
                     </Link>
                   </div>
                 );
